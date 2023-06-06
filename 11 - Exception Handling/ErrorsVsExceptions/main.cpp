50982f268cc7cf428feea479c89e338b978fd82e
Charlie Lambert <charlie.lambert@gmail.com>
2023-06-05 23:32:48 -0600

    Exception Handling

---
+++ 11 - Exception Handling/ErrorsVsExceptions/main.cpp
@@ -0,0 +1,43 @@
+/*
+    What
+    Errors vs Exception
+
+    Why
+    Bad things happen, we need to know how to handle them
+
+    How
+    An Error
+    "indicates serious problmes that a reasonable
+    application shouldnot try to catch." - bad things
+
+    An Exception
+    "indicates conditions that a reasonable
+    application might want to catch." - most things
+
+    Warning: people use these interchangeably
+*/
+
+#include <QCoreApplication>
+#include <QDebug>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    do {
+        int max = 44;
+        int div = 0;
+
+        qInfo()<< "Enter a number to divide by (Or Zero for an issue!)";
+        cin >> div;
+
+        int answer = max / div; // Possible Exception
+
+        qInfo() << answer;
+    } while (true);
+
+    return a.exec();
+}
